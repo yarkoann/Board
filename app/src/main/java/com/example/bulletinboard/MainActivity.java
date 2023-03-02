e3688009f7eef2491c61029d68e834122b66921b
yarkoann <a_yar23@mail.ru>
2023-03-02 11:44:38 +0900

    новый

---
+++ app/src/main/java/com/example/bulletinboard/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.bulletinboard;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private NavigationView nav_view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init()
+    {
+        nav_view = findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(this);
+
+        // test firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World!");
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id)
+        {
+            case R.id.id_my_ads:
+                Toast.makeText(this, "Pressed id my ads", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.id_car_ads:
+                Toast.makeText(this, "Pressed id cars", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.id_pc_ads:
+                Toast.makeText(this, "Pressed id pc", Toast.LENGTH_SHORT).show();
+                    break;
+            case R.id.id_phone_ads:
+                Toast.makeText(this, "Pressed id phone", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.id_dm_ads:
+                Toast.makeText(this, "Pressed id dm", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.id_sign_up:
+                signDialog(R.string.sign_up, R.string.sign_up_button);
+                Toast.makeText(this, "Pressed id sign up", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.id_sign_in:
+                signDialog(R.string.sign_in, R.string.sign_in_button);
+                Toast.makeText(this, "Pressed id sign in", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.id_sign_out:
+                Toast.makeText(this, "Pressed id sign out", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
+    private void signDialog(int title, int buttonTitle)
+    {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.sign_up_layout, null);
+        dialogBuilder.setView(dialogView);
+
+        TextView titleTextView = dialogView.findViewById(R.id.tvAlertTitle);
+        titleTextView.setText(title);
+
+        Button butt = dialogView.findViewById(R.id.buttonSignUp);
+        butt.setText(buttonTitle);
+        butt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
+    }
+}

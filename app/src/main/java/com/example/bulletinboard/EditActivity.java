0f98d48da0416e117e2909b0028e90e00933020b
yarkoann <a_yar23@mail.ru>
2023-03-21 13:03:54 +0900

    картинка

--- app/src/main/java/com/example/bulletinboard/EditActivity.java
+++ app/src/main/java/com/example/bulletinboard/EditActivity.java
@@ -1,14 +1,47 @@
 package com.example.bulletinboard;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class EditActivity extends AppCompatActivity {
+    private ImageView imItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        init();
+        imItem.setOnClickListener(view -> {
+            getImage();
+        });
+    }
+    private  void init()
+    {
+        imItem = findViewById(R.id.imItem);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 10 && resultCode == RESULT_OK && data != null)
+        {
+            Uri imageUri = data.getData();
+            imItem.setImageURI(imageUri);
+        }
+    }
+
+    private void getImage()
+    {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, 10);
     }
 }

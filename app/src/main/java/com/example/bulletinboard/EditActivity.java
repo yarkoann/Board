6965ab3bc21849dc81dbdf1d3febb20ab62e2f38
yarkoann <a_yar23@mail.ru>
2023-03-27 13:55:14 +0900

    каринки2

--- app/src/main/java/com/example/bulletinboard/EditActivity.java
+++ app/src/main/java/com/example/bulletinboard/EditActivity.java
@@ -1,18 +1,36 @@
 package com.example.bulletinboard;
 
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 public class EditActivity extends AppCompatActivity {
+
+    private StorageReference mStorageRef;
     private ImageView imItem;
+
+    private Uri uploadUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +43,7 @@ public class EditActivity extends AppCompatActivity {
     }
     private  void init()
     {
+        mStorageRef = FirebaseStorage.getInstance().getReference("Images");
         imItem = findViewById(R.id.imItem);
     }
 
@@ -35,7 +54,35 @@ public class EditActivity extends AppCompatActivity {
         {
             Uri imageUri = data.getData();
             imItem.setImageURI(imageUri);
+            uploadImage();
         }
+    }
+    private void uploadImage()
+    {
+        Bitmap bitmap = ((BitmapDrawable)imItem.getDrawable()).getBitmap();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100, out);
+        byte[] byteArray = out.toByteArray();
+        final StorageReference mRef = mStorageRef.child(System.currentTimeMillis() + "_image");
+        UploadTask up = mRef.putBytes(byteArray);
+        Task<Uri> task = up.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                return mRef.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+            uploadUri = task.getResult();
+                Toast.makeText(EditActivity.this, "Upload done: " + uploadUri.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        })
+;
     }
 
     private void getImage()
